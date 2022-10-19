f9df0c8ab0fe4e862faa678fcc63dfc6d543c1d1
sTEVE31-bAY <stephenmoses399@gmail.com>
2022-10-19 14:49:51 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,10 @@
  *
  * Return: 1 if character is a letter,0 otherwise
  */
-
 int _isalpha(int c)
-{ 
+{
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-	else 
+	else
 		return (0);
 }
