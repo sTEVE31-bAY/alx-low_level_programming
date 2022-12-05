15924a2842c800bced9fbc9d50ffc97a8b862c2b
sTEVE31-bAY <stephenmoses399@yahoo.com>
2022-12-05 03:37:51 -0800

    Task 1 correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Create_file - Creates a file.
+ * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
@@ -11,6 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
 
