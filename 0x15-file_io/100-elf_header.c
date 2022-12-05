51ec4810bb88b77e8e429cf162bd308fc4762360
sTEVE31-bAY <stephenmoses399@yahoo.com>
2022-12-05 04:07:01 -0800

    Additional task correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -117,7 +117,7 @@ void print_data(unsigned char *e_ident)
 void print_osabi(unsigned char *e_ident)
 {
 	printf("  OS/ABI:                            ");
-	
+
 	switch (e_ident[EI_OSABI])
 	{
 		case ELFOSABI_NONE:
@@ -154,25 +154,6 @@ void print_osabi(unsigned char *e_ident)
 			printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
 }
-
-/**
- *
- *  * print_abi - Prints the ABI version of an ELF header.
- *
- *   * @e_ident: A pointer to an array containing the ELF ABI version.
- *
- *    */
-
-void print_abi(unsigned char *e_ident)
-
-{
-
-		printf("  ABI Version:                       %d\n",
-
-					       e_ident[EI_ABIVERSION]);
-
-}
-
 
 /**
  * print_abi - Prints the ABI version of an ELF header.
